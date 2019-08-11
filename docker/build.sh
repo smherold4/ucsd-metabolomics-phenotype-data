38daef2cc0e55b4769a04b79c02b59d7c5f9eb02
Michael Herold <herold4@gmail.com>
2019-08-12 00:13:11 +0200

    no cache

--- docker/build.sh
+++ docker/build.sh
@@ -1,9 +1,9 @@
 
-docker build -f ./docker/kibana/Dockerfile -t herold961/ucsd-kibana ./docker/kibana
+docker build --no-cache -f ./docker/kibana/Dockerfile -t herold961/ucsd-kibana ./docker/kibana
 docker push herold961/ucsd-kibana
 
-docker build -f ./docker/nas/Dockerfile -t herold961/ucsd-nas ./docker/nas
+docker build --no-cache -f ./docker/nas/Dockerfile -t herold961/ucsd-nas ./docker/nas
 docker push herold961/ucsd-nas
 
-docker build -f ./docker/workstation/Dockerfile -t herold961/ucsd-workstation ./docker/workstation
+docker build --no-cache -f ./docker/workstation/Dockerfile -t herold961/ucsd-workstation ./docker/workstation
 docker push herold961/ucsd-workstation
