ebe1c98d05ba2204f081f1421176ba704a2b757a
Michael Herold <herold4@gmail.com>
2019-08-11 23:52:55 +0200

    docker build path

--- docker/build.sh
+++ docker/build.sh
@@ -1,9 +1,9 @@
 
-docker build -f ./kibana/Dockerfile -t herold961/ucsd-kibana
+docker build -f ./kibana/Dockerfile -t herold961/ucsd-kibana ./kibana
 docker push herold961/ucsd-kibana
 
-docker build -f ./nas/Dockerfile -t herold961/ucsd-nas
+docker build -f ./nas/Dockerfile -t herold961/ucsd-nas ./nas
 docker push herold961/ucsd-nas
 
-docker build -f ./workstation/Dockerfile -t herold961/ucsd-workstation
+docker build -f ./workstation/Dockerfile -t herold961/ucsd-workstation ./workstation
 docker push herold961/ucsd-workstation
