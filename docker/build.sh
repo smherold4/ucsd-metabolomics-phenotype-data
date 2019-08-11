83eb62383990801c9daac5f991351491ce76c895
Michael Herold <herold4@gmail.com>
2019-08-11 23:49:41 +0200

    shell script to build

---
+++ docker/build.sh
@@ -0,0 +1,9 @@
+
+docker build -f ./kibana/Dockerfile -t herold961/ucsd-kibana
+docker push herold961/ucsd-kibana
+
+docker build -f ./nas/Dockerfile -t herold961/ucsd-nas
+docker push herold961/ucsd-nas
+
+docker build -f ./workstation/Dockerfile -t herold961/ucsd-workstation
+docker push herold961/ucsd-workstation
