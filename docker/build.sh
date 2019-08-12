de5f89880f75582ef574dc656e672815b9461d12
Michael Herold <herold4@gmail.com>
2019-08-12 12:12:27 +0200

    password

--- docker/build.sh
+++ docker/build.sh
@@ -1,9 +1,14 @@
 
+if [ -z "$ELASTIC_PASSWORD" ] ; then
+  echo "Env variable ELASTIC_PASSWORD required" 1>&2
+  exit 64
+fi
+
 docker build --no-cache -f ./docker/kibana/Dockerfile -t herold961/ucsd-kibana ./docker/kibana
 docker push herold961/ucsd-kibana
 
-docker build --no-cache -f ./docker/nas/Dockerfile -t herold961/ucsd-nas ./docker/nas
+docker build --no-cache -f ./docker/nas/Dockerfile -t herold961/ucsd-nas ./docker/nas --build-arg ELASTIC_PASSWORD=$ELASTIC_PASSWORD
 docker push herold961/ucsd-nas
 
-docker build --no-cache -f ./docker/workstation/Dockerfile -t herold961/ucsd-workstation ./docker/workstation
+docker build --no-cache -f ./docker/workstation/Dockerfile -t herold961/ucsd-workstation ./docker/workstation --build-arg ELASTIC_PASSWORD=$ELASTIC_PASSWORD
 docker push herold961/ucsd-workstation
