92aef2c2e637690ce7c216d2782b7cf60be8ff0e
Michael Herold <herold4@gmail.com>
2019-08-12 13:19:12 +0200

    certs

--- docker/build.sh
+++ docker/build.sh
@@ -7,8 +7,8 @@ fi
 docker build --no-cache -f ./docker/kibana/Dockerfile -t herold961/ucsd-kibana ./docker/kibana
 docker push herold961/ucsd-kibana
 
-docker build --no-cache -f ./docker/nas/Dockerfile -t herold961/ucsd-nas ./docker/nas --build-arg ELASTIC_PASSWORD=$ELASTIC_PASSWORD --build-arg CERTS_DIR=$CERTS_DIR
+docker build --no-cache -f ./docker/nas/Dockerfile -t herold961/ucsd-nas ./docker/nas --build-arg ELASTIC_PASSWORD=$ELASTIC_PASSWORD
 docker push herold961/ucsd-nas
 
-docker build --no-cache -f ./docker/workstation/Dockerfile -t herold961/ucsd-workstation ./docker/workstation --build-arg ELASTIC_PASSWORD=$ELASTIC_PASSWORD --build-arg CERTS_DIR=$CERTS_DIR
+docker build --no-cache -f ./docker/workstation/Dockerfile -t herold961/ucsd-workstation ./docker/workstation --build-arg ELASTIC_PASSWORD=$ELASTIC_PASSWORD
 docker push herold961/ucsd-workstation
