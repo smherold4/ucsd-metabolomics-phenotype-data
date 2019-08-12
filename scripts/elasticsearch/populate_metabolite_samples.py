eb8b1b10c56d85bcd87c5e1cfd8620844792dc4b
Michael Herold <herold4@gmail.com>
2019-08-12 08:54:22 +0200

    last queries id

--- scripts/elasticsearch/populate_metabolite_samples.py
+++ scripts/elasticsearch/populate_metabolite_samples.py
@@ -20,7 +20,7 @@ def run(args):
     session = db_connection.session_factory()
     cohort = session.query(Cohort).filter(Cohort.name == args.cohort_name).first()
     assert cohort is not None, "Could not find cohort with name '{}'".format(args.cohort_name)
-    line_count = 0
+    line_count = args.starting_entity_id or 0
 
     last_queried_id = 0
     while last_queried_id is not None:
