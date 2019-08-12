24261fd881b9a8f57ce9722e3e9b6954f01f3218
Michael Herold <herold4@gmail.com>
2019-08-12 11:23:42 +0200

    require env variable

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,9 @@
 
+if [ -z "$ELASTIC_PASSWORD" ] ; then
+  echo "Env variable ELASTIC_PASSWORD required" 1>&2
+  exit 64
+fi
+
 docker build --no-cache -f ./docker/kibana/Dockerfile -t herold961/ucsd-kibana ./docker/kibana
 docker push herold961/ucsd-kibana
 
