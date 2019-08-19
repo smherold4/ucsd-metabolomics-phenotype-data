69366278478950db5cfc89bb2b14d90d8125c114
Michael Herold <herold4@gmail.com>
2019-08-19 17:42:47 +0200

    populate_sample_phenotypes

--- scripts/elasticsearch/__init__.py
+++ scripts/elasticsearch/__init__.py
@@ -1,11 +1,13 @@
 import populate_metabolite_samples
 import populate_metabolite_alignments
 import populate_subject_phenotypes
+import populate_sample_phenotypes
 import populate_phenotype_descriptions
 
 __all__ = [
   'populate_metabolite_samples',
   'populate_metabolite_alignments',
+  'populate_sample_phenotypes',
   'populate_subject_phenotypes',
   'populate_phenotype_descriptions',
 ]
