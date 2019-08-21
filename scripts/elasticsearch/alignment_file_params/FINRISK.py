4666bae679547f2e23ed7956c65ed7332e9213eb
Michael Herold <herold4@gmail.com>
2019-08-21 12:28:17 +0200

    alignments

--- scripts/elasticsearch/alignment_file_params/FINRISK.py
+++ scripts/elasticsearch/alignment_file_params/FINRISK.py
@@ -8,5 +8,20 @@ files = [
     "path": "/volume1/Jain Lab Data/MassSpecDatabase/Eicosanoid method/FINRISK/AlignedPeaks_FINRISK_MESA.csv",
     "cohort_name": "MESA",
     "cohort_column": "B",
+  },
+  {
+    "path": "/volume1/Jain Lab Data/MassSpecDatabase/Eicosanoid method/FINRISK/AlignedPeaks_MESA04_FINRISK.csv",
+    "cohort_name": "MESA",
+    "cohort_column": "A",
+  },
+  {
+    "path": "/volume1/Jain Lab Data/MassSpecDatabase/Eicosanoid method/FINRISK/AlignedPeaks_FINRISK_Vital 400.csv",
+    "cohort_name": "VITAL 400",
+    "cohort_column": "B",
+  },
+  {
+    "path": "/volume1/Jain Lab Data/MassSpecDatabase/Eicosanoid method/FINRISK/AlignedPeaks_FINRISK_Vital CTSC.csv",
+    "cohort_name": "VITAL CTSC",
+    "cohort_column": "B",
   }
 ]
