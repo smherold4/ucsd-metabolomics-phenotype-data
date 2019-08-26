e64a7bf77fe9fcdc56900c609b89bdeced505e0d
Michael Herold <herold4@gmail.com>
2019-08-26 16:05:01 +0200

    align CIAO

---
+++ scripts/elasticsearch/alignment_file_params/CIAO_SAGE.py
@@ -0,0 +1,32 @@
+files = [
+  {
+    "path": "/volume1/Jain Lab Data/MassSpecDatabase/Eicosanoid method/CIAO-SAGE/AlignedPeaks_CIAO-SAGE_FHS.csv",
+    "cohort_name": "FHS",
+    "cohort_column": "B",
+  },
+  {
+    "path": "/volume1/Jain Lab Data/MassSpecDatabase/Eicosanoid method/CIAO-SAGE/AlignedPeaks_CIAO-SAGE_FINRISK.csv",
+    "cohort_name": "FINRISK",
+    "cohort_column": "B",
+  },
+  {
+    "path": "/volume1/Jain Lab Data/MassSpecDatabase/Eicosanoid method/CIAO-SAGE/AlignedPeaks_CIAO-SAGE_MESA02.csv",
+    "cohort_name": "MESA",
+    "cohort_column": "B",
+  },
+  {
+    "path": "/volume1/Jain Lab Data/MassSpecDatabase/Eicosanoid method/CIAO-SAGE/AlignedPeaks_CIAO-SAGE_MESA04.csv",
+    "cohort_name": "MESA",
+    "cohort_column": "B",
+  },
+  {
+    "path": "/volume1/Jain Lab Data/MassSpecDatabase/Eicosanoid method/CIAO-SAGE/AlignedPeaks_CIAO-SAGE_Vital 400.csv",
+    "cohort_name": "VITAL 400",
+    "cohort_column": "B",
+  },
+  {
+    "path": "/volume1/Jain Lab Data/MassSpecDatabase/Eicosanoid method/CIAO-SAGE/AlignedPeaks_CIAO-SAGE_Vital CTSC.csv",
+    "cohort_name": "VITAL CTSC",
+    "cohort_column": "B",
+  }
+]
