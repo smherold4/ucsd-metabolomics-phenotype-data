ac2488b41ddaea8f6bbf96bd340a2ccb108909b5
Michael Herold <herold4@gmail.com>
2019-08-26 16:20:02 +0200

    comma

--- scripts/elasticsearch/alignment_file_params/VITAL_400.py
+++ scripts/elasticsearch/alignment_file_params/VITAL_400.py
@@ -28,5 +28,5 @@ files = [
     "path": "/volume1/Jain Lab Data/MassSpecDatabase/Eicosanoid method/VITAL 400/AlignedPeaks_CIAO-SAGE_Vital 400.csv",
     "cohort_name": "CIAO-SAGE",
     "cohort_column": "A",
-  }
+  },
 ]
