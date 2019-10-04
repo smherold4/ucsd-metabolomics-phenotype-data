14454539326f45f2908a10b25ab2071cb6aa05e5
Michael Herold <herold4@gmail.com>
2019-10-04 15:22:18 -0700

    update imports

--- scripts/elasticsearch/__init__.py
+++ scripts/elasticsearch/__init__.py
@@ -6,14 +6,4 @@ import populate_phenotype_descriptions
 import populate_microbiome_abundances
 import populate_microbiome_alignments
 import populate_microbiome_sequences
-
-__all__ = [
-    'populate_metabolite_samples',
-    'populate_metabolite_alignments',
-    'populate_sample_phenotypes',
-    'populate_subject_phenotypes',
-    'populate_phenotype_descriptions',
-    'populate_microbiome_abundances',
-    'populate_microbiome_alignments',
-    'populate_microbiome_sequences',
-]
+import populate_microbiome_sample_phenotypes
